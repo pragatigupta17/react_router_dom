aa8ab5ef8bc32e629fea94a8c5481436a5d3841e
Pragati Gupta <pragatigupta0104@gmail.com>
2025-01-09 13:05:04 +0530

    db

---
+++ AXIOS/src/Fet.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect,useState } from "react";
+import axios from 'axios';
+
+const Fet =()=>{
+    let [jsondata,setData]=useState([]);
+    const mydel=(id)=>{
+        axios.delete(`http://localhost:3000/student/${id}`)
+        .then(re=>alert("deleted")) 
+    }
+
+    useEffect(()=>{
+        axios.get('http://localhost:3000/student')
+        .then(res=> {
+            console.log(res.data);
+            setData(res.data)
+            })
+
+    },[mydel])
+    return(
+        <>
+        <table border="">
+            <thead>
+
+                <th>ID</th>
+                <th>NAME</th>
+                <th>AGE</th>
+                <th>CONTACT</th>
+                <th>CITY</th>
+                <th>DELETE</th>
+                
+            </thead>
+            <tbody>
+                {
+                    jsondata.map((e)=>(
+                        <tr>
+                            <td>{e.id}</td>
+                            <td>{e.name}</td>
+                            <td>{e.age}</td>
+                            <td>{e.contact}</td>
+                            <td>{e.city}</td>
+                            <td><button onClick={()=>mydel(e.id)}>DELETE</button></td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+        </>
+    )
+}
+export default Fet
