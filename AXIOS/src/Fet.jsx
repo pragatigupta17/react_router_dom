7f9dd9749862e3772d7957f6605dc20ac13f6c34
Pragati Gupta <pragatigupta0104@gmail.com>
2025-01-10 12:36:04 +0530

    form

--- AXIOS/src/Fet.jsx
+++ AXIOS/src/Fet.jsx
@@ -3,6 +3,21 @@ import axios from 'axios';
 
 const Fet =()=>{
     let [jsondata,setData]=useState([]);
+    let [frminp,setFrminp]=useState({
+        name:"",
+        age:"",
+        contact:"",
+        city:"",
+    })
+    const Changeinp =(e)=>{
+        let {name,value}=e.target;
+        setFrminp({...frminp,[name]:value})
+    }
+    const submitform =(e)=>{
+        e.preventDefault()
+        axios.post('http://localhost:3000/student',frminp)
+        .then(r=>alert("inserted"))
+    }
     const mydel=(id)=>{
         axios.delete(`http://localhost:3000/student/${id}`)
         .then(re=>alert("deleted")) 
@@ -44,6 +59,17 @@ const Fet =()=>{
                 }
             </tbody>
         </table>
+        <form onSubmit={submitform}>
+            <label htmlFor="">Name</label>            
+            <input type="text" value={frminp.name} name="name" placeholder="name" onChange={Changeinp}/><br />
+            <label htmlFor="">Age</label>      
+            <input type="text" value={frminp.age} name="age" placeholder="age" onChange={Changeinp}/><br />
+            <label htmlFor="">Contact</label>      
+            <input type="text" value={frminp.contact} name="contact" placeholder="contact" onChange={Changeinp}/><br />
+            <label htmlFor="">City</label>      
+            <input type="text" value={frminp.city} name="city" placeholder="city" onChange={Changeinp}/><br />
+            <button type="submit">SUBMIT</button>
+        </form>
         </>
     )
 }
